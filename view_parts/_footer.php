4ed2d2e29401cfc815f33e3265b28b80713a30c5
fmeniass <massomflavy@yahoo.fr>
2016-02-25 08:45:39 -0500

    edit footer

--- view_parts/_footer.php
+++ view_parts/_footer.php
@@ -1,3 +1,10 @@
 <div>footer ici</div>
+<form action="#">
+    <input type="text" name="message" placeholder="Laisser nous un message">
+    <input type="submit" value="envoyer">
+    <input type="search" name="recherche" placeholder="Recherche">
+    <input type="submit" value="valider">
+</form>
+<p>&copy; CN " La gare de rires" 2016</p>
 </body>
 </html>
