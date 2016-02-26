c4ad81027a51c435b098ffaf1407e3752787c59f
fmeniass <massomflavy@yahoo.fr>
2016-02-26 10:42:18 -0500

    modification css mobile équipe

--- view_parts/_footer.php
+++ view_parts/_footer.php
@@ -1,11 +1,11 @@
-
+<div id="footer">
 <form action="#">
-    <!--<input type="text" name="message" placeholder="Laisser nous un message">-->
     <textarea rows="5" cols="30" placeholder="Laisser nous un message"></textarea>
     <input type="submit" value="envoyer">
     <input type="search" name="recherche" placeholder="Recherche">
     <input type="submit" value="valider">
 </form>
+</div>
 <p> &copy; CN " La gare de rires" 2016 </p>
 </body>
 </html>
