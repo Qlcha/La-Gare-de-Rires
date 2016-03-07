5e6b6cb8285a27b14ddc11bbbcca1f76c3e3295d
qlcha <olga.sphere@gmail.com>
2016-03-06 23:13:54 -0500

    Page Galerie - images uploaded

--- gallery.php
+++ gallery.php
@@ -8,43 +8,55 @@ require_once 'view_parts/_header.php';
 
 <article class="preview">
     <h3>Activités divers</h3>
-<a class="fancybox" rel="gallery1" title="Gallery 1 - 1" href="images/galerie/activités%20divers/IMG_0001.jpg"><img  src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+<a class="fancybox" rel="gallery1" title="Activités divers - 1" href="images/galerie/activités%20divers/SDC11098.JPG"><img  src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
 </article>
 <div class="hidden">
-    <a class="fancybox" rel="gallery1" title="Gallery 1 - 2" href="images/galerie/activités%20divers/SDC10914.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
-
-    <a class="fancybox" rel="gallery1" title="Gallery 1 - 3" href="images/galerie/activités%20divers/SDC10930.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+    <a class="fancybox" rel="gallery1" title="Activités divers - 2" href="images/galerie/activités%20divers/SDC10935.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+    <a class="fancybox" rel="gallery1" title="Activités divers - 3" href="images/galerie/activités%20divers/SDC10930.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+    <a class="fancybox" rel="gallery1" title="Activités divers - 4" href="images/galerie/activités%20divers/SDC11041.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+    <a class="fancybox" rel="gallery1" title="Activités divers - 5" href="images/galerie/activités%20divers/unnamed.jpg"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+    <a class="fancybox" rel="gallery1" title="Activités divers - 6" href="images/galerie/activités%20divers/SDC10944.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+    <a class="fancybox" rel="gallery1" title="Activités divers - 7" href="images/galerie/activités%20divers/SDC11002.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
 </div>
 
     <article class="preview">
         <h3>Pouponniere</h3>
-        <a class="fancybox" rel="gallery1" title="Gallery 1 - 1" href="images/galerie/activités%20divers/IMG_0001.jpg"><img  src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Pouponniere - 1" href="images/galerie/pouponerrie/IMG_5739.JPG"><img  src="images/galerie/pouponerrie/IMG_5739.JPG" alt=""/></a>
     </article>
     <div class="hidden">
-        <a class="fancybox" rel="gallery1" title="Gallery 1 - 2" href="images/galerie/activités%20divers/SDC10914.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Pouponniere - 2" href="images/galerie/pouponerrie/IMG_5744.JPG"><img src="images/galerie/pouponerrie/IMG_5739.JPG" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Pouponniere - 3" href="images/galerie/pouponerrie/IMG_5756.JPG"><img src="images/galerie/pouponerrie/IMG_5739.JPG" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Pouponniere - 4" href="images/galerie/pouponerrie/SDC10851.JPG"><img src="images/galerie/pouponerrie/IMG_5739.JPG" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Pouponniere - 5" href="images/galerie/pouponerrie/IMG_5771.JPG"><img src="images/galerie/pouponerrie/IMG_5739.JPG" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Pouponniere - 6" href="images/galerie/pouponerrie/IMG_5773.JPG"><img src="images/galerie/pouponerrie/IMG_5739.JPG" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Pouponniere - 7" href="images/galerie/pouponerrie/IMG_5753.JPG"><img src="images/galerie/pouponerrie/IMG_5739.JPG" alt=""/></a>
 
-        <a class="fancybox" rel="gallery1" title="Gallery 1 - 3" href="images/galerie/activités%20divers/SDC10930.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
     </div>
 
 
     <article class="preview">
         <h3>Halloween 2015</h3>
-        <a class="fancybox" rel="gallery1" title="Gallery 1 - 1" href="images/galerie/activités%20divers/IMG_0001.jpg"><img  src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Halloween 2015 - 1" href="images/galerie/Halloween%202014/2014-10-31 18.58.33.jpg"><img  src="images/galerie/Halloween%202014/2014-10-31 18.58.33.jpg" alt=""/></a>
     </article>
     <div class="hidden">
-        <a class="fancybox" rel="gallery1" title="Gallery 1 - 2" href="images/galerie/activités%20divers/SDC10914.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
-
-        <a class="fancybox" rel="gallery1" title="Gallery 1 - 3" href="images/galerie/activités%20divers/SDC10930.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Halloween 2015 - 2" href="images/galerie/Halloween%202014/2014-10-31%2009.05.01.jpg"><img src="images/galerie/Halloween%202014/2014-10-31 18.58.33.jpg" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Halloween 2015 - 3" href="images/galerie/Halloween%202014/2014-10-31 19.01.46.jpg"><img src="images/galerie/Halloween%202014/2014-10-31 18.58.33.jpg" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Halloween 2015 - 4" href="images/galerie/Halloween%202014/2014-10-31 19.02.28.jpg"><img src="images/galerie/Halloween%202014/2014-10-31 18.58.33.jpg" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Halloween 2015 - 5" href="images/galerie/Halloween%202014/2014-10-31 19.18.50.jpg"><img src="images/galerie/Halloween%202014/2014-10-31 18.58.33.jpg" alt=""/></a>
     </div>
 
     <article class="preview">
         <h3>Au pay du Père Noël</h3>
-        <a class="fancybox" rel="gallery1" title="Gallery 1 - 1" href="images/galerie/activités%20divers/IMG_0001.jpg"><img  src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Au pay du Père Noël - 1" href="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/705.jpg"><img  src="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/705.jpg" alt=""/></a>
     </article>
     <div class="hidden">
-        <a class="fancybox" rel="gallery1" title="Gallery 1 - 2" href="images/galerie/activités%20divers/SDC10914.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Au pay du Père Noël - 2" href="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/706.jpg"><img src="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/705.jpg" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Au pay du Père Noël - 3" href="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/707.jpg"><img src="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/705.jpg" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Au pay du Père Noël - 4" href="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/708.jpg"><img src="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/705.jpg" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Au pay du Père Noël - 5" href="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/709.jpg"><img src="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/705.jpg" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Au pay du Père Noël - 6" href="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/704.jpg"><img src="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/705.jpg" alt=""/></a>
+        <a class="fancybox" rel="gallery1" title="Au pay du Père Noël - 7" href="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/710.jpg"><img src="images/galerie/sorti%20au%20pay%20du%20Pere%20Noel/705.jpg" alt=""/></a>
 
-        <a class="fancybox" rel="gallery1" title="Gallery 1 - 3" href="images/galerie/activités%20divers/SDC10930.JPG"><img src="images/galerie/activités%20divers/SDC11092.JPG" alt=""/></a>
     </div>
 
 
